3f71e20f6efcd542fe975b97905c6f70c0ace6c5
mohaelmi <moha.abdi1771@gmail.com>
2023-05-20 01:10:40 -0500

    fixed with eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,16 @@
 
 const assertEqual = function(actual, expected) {
-  if((actual === expected)) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`)
-  }else if((actual !== expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  if ((actual === expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else if ((actual !== expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  if((actual != expected)) {
-    console.log(`🛑🛑🛑 Assertion Fialed: ${actual} != ${expected}`)
+  if ((actual != expected)) {
+    console.log(`🛑🛑🛑 Assertion Fialed: ${actual} != ${expected}`);
   }
 
-}
+};
 
 
-assertEqual('Lighthouse Labs', 'bootcamp')
-assertEqual(1, 1)
+assertEqual('Lighthouse Labs', 'bootcamp');
+assertEqual(1, 1);
