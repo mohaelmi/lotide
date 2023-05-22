67ad1f64b10723d2726ae96ef2db97e8a248532c
mohaelmi <moha.abdi1771@gmail.com>
2023-05-21 22:29:29 -0500

    added eqArrays function with assert test

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if ((actual === expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else if ((actual !== expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  // if ((actual != expected)) {
+  //   console.log(`🛑🛑🛑 Assertion Fialed: ${actual} != ${expected}`);
+  // }
+
+};
+
+const eqArrays = function(array1, array2) {
+  if(array1.length !== array2.length) return false;
+  else {
+    for(let i in array1){
+      if(array1[i] !== array2[i]) return false
+    }
+  }
+  return true
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
