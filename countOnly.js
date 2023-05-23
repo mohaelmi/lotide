4aedcf63a9654ec767793f310f4c78ab71a93b9a
mohaelmi <moha.abdi1771@gmail.com>
2023-05-23 10:43:58 -0500

    added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+
+const assertEqual = function(actual, expected) {
+  if ((actual === expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else if ((actual !== expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if ((actual != expected)) {
+    console.log(`🛑🛑🛑 Assertion Fialed: ${actual} != ${expected}`);
+  }
+
+};
+
+
+const countOnly = function(allItems, itemToCount) { 
+  const results = {}
+  for(const item of allItems){
+    console.log(itemToCount[item])
+    if(itemToCount[item] === true){ 
+      if(results[item]){
+        results[item] += 1
+      }else {
+        results[item] = 1
+
+      }
+    }
+  }
+
+  return results
+  
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Fang",
+  "Salima",
+  "Joe"
+];
+
+let result1  = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+ console.log(result1)
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
