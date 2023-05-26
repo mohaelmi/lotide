46b189bfd4a9eb5d6f2fac1721dbb09f9d5b65c3
mohaelmi <moha.abdi1771@gmail.com>
2023-05-26 02:16:07 -0500

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if ((actual === expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else if ((actual !== expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const findKey = function(obj, callback) {
+  
+  for(let key in obj){
+    if(callback(key)) return key
+  }
+
+  return undefined;
+}
+
+const data = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+const result = findKey(data, (key) => {
+  return data[key].stars === 2
+})
+
+const result1 = findKey(data, (key) => {
+  return data[key].stars === 8
+})
+
+
+//test cases
+assertEqual(result, 'noma') // => Pass
+assertEqual(result1, undefined) // => Pass
+assertEqual(result, 'Ora') // => Fail
